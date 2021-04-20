1446669943112c2a7969c667e36c888ef63a91a7
强子 <qiangzi@via.cool>
2021-04-20 18:49:57 +0800

    提交

--- src/Page.js
+++ src/Page.js
@@ -1,7 +1,12 @@
 import React from "react";
+import data from "../raws/2021-04-20/1.json";
+
 class Page extends React.Component {
 	constructor() {
 		super();
+		this.state = {
+			data,
+		};
 	}
 
 	render() {
